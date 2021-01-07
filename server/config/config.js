edc21bdf5d8ea2ddae395b011ca3a5829d0bd260
abdulhakim2902 <abdulhakim2902@gmail.com>
2021-01-07 23:22:44 +0700

    server

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,12 @@
-{
+require('dotenv').config();
+
+module.exports = {
   "development": {
-    "username": "postgres",
-    "password": "postgres",
-    "database": "healthylyfe_database_development",
-    "host": "localhost",
-    "dialect": "postgres"
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT
   },
   "test": {
     "username": "root",
