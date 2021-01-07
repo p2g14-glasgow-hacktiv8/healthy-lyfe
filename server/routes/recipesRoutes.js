78a225a5bbe61813b145ff6c765a9d77c3de6c10
agungmrizqon <agungrizqon18@gmail.com>
2021-01-07 22:26:04 +0700

    client_+_static_loginGoogle

--- server/routes/recipesRoutes.js
+++ server/routes/recipesRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const RecipeController = require('../controllers/recipe-controller')
+// const RecipeController = require('../controllers/recipe-controller')
 
 router.get()
 router.delete()
