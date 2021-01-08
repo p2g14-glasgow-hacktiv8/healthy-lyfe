4b4e401d5b465d772c223f892046f1915eb640b8
Muhammad Abdul Hakim Shibghatallah <70675129+abdulhakim2902@users.noreply.github.com>
2021-01-08 08:49:28 +0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 3000
 const router = require('./routes')
 const error = require('./middlewares/error')
 const cors = require('cors')
